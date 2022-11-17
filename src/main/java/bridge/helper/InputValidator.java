1a1eeff54fad3bfb340f32244a8b219a71f15ae5
ParkJiwoon <bcp0109@gmail.com>
2022-11-17 23:54:42 +0900

    refactor: input validator 를 instance 로 변경

--- src/main/java/bridge/helper/InputValidator.java
+++ src/main/java/bridge/helper/InputValidator.java
@@ -3,8 +3,10 @@ package bridge.helper;
 import java.util.Set;
 
 public class InputValidator {
+    public static final int MIN_BRIDGE_SIZE = 3;
+    public static final int MAX_BRIDGE_SIZE = 20;
 
-    public static void validateNumberType(String input) {
+    public void validateNumberType(String input) {
         boolean isNumber = input.chars().allMatch(Character::isDigit);
 
         if (!isNumber) {
@@ -12,13 +14,13 @@ public class InputValidator {
         }
     }
 
-    public static void validateMove(String input) {
+    public void validateMove(String input) {
         if (!Set.of("U", "D").contains(input)) {
             throw new IllegalArgumentException("[ERROR] 이동할 칸은 U 또는 D 만 입력할 수 있습니다.");
         }
     }
 
-    public static void validateRetryOrQuit(String input) {
+    public void validateRetryOrQuit(String input) {
         if (!Set.of("R", "Q").contains(input)) {
             throw new IllegalArgumentException("[ERROR] 재시도 선택은 R 또는 Q 만 입력할 수 있습니다.");
         }
--- src/main/java/bridge/helper/InputView.java
+++ src/main/java/bridge/helper/InputView.java
@@ -6,6 +6,11 @@ import camp.nextstep.edu.missionutils.Console;
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private final InputValidator inputValidator;
+
+    public InputView(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
 
     /**
      * 다리의 길이를 입력받는다.
@@ -16,7 +21,7 @@ public class InputView {
         System.out.println("다리의 길이를 입력해주세요.");
 
         String input = Console.readLine();
-        InputValidator.validateNumberType(input);
+        inputValidator.validateNumberType(input);
         return Integer.parseInt(input);
     }
 
@@ -27,7 +32,7 @@ public class InputView {
         System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
 
         String input = Console.readLine();
-        InputValidator.validateMove(input);
+        inputValidator.validateMove(input);
         return input;
     }
 
@@ -38,7 +43,7 @@ public class InputView {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
 
         String input = Console.readLine();
-        InputValidator.validateRetryOrQuit(input);
+        inputValidator.validateRetryOrQuit(input);
         return input;
     }
 }
